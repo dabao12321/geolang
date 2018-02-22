aad9a94f9fef7687fa1fdb6018e563f69933be15
Amanda Li <amli7@comcast.net>
2018-02-22 16:43:51 -0500

    added inits and relative importing

--- src/classifiers/vectorize_corpus.py
+++ src/classifiers/vectorize_corpus.py
@@ -3,9 +3,15 @@ import numpy as np
 import pandas as pd
 import nltk
 import os
+import sys
+sys.path.append('../')
+from cleaners.clean_geodata import abbrev_to_state
 
 
 def get_catagories():
+    '''
+    Return a standard list of regional catagories (states)
+    '''
     catagories = []
     geodata = pd.read_csv("../../data/cleaned_dare_corpus.csv")
     for x in geodata['dialect']:
@@ -32,7 +38,10 @@ def read_DARE():
     return examples, targets, catagories
 
 def read_SB_transcriptions():
-    '''Load all transcription .csv data into one DataFrame
+    '''
+    Read in SB translations from cleaned CSVs, returns data points and target lists.
+    Data points are words.
+    Targets (states) correspond to the index of said state in catagories.
     '''
     directory = "../../data/SBData/dialectsdata"
     transcripts = pd.DataFrame({"name":[], "dialect":[], "word":[]})
@@ -47,3 +56,19 @@ def read_SB_transcriptions():
             continue
     transcripts = transcripts.reset_index()
     transcripts = transcripts.drop("index", axis = 1)
+
+    SBexamples = []
+    SBtargets = []
+    catagories = get_catagories()
+    for i,x in enumerate(transcripts['word']):
+        if pd.notnull(x) and pd.notnull(transcripts.get_value(i, 'dialect')):
+            try:
+                dialect = transcripts.get_value(i,'dialect')
+                dialect = abbrev_to_state(dialect)
+                target = catagories.index(dialect)
+                SBexamples.append(x)
+                SBtargets.append(target)
+            except KeyError:
+                # These speakers do not identify with a single state, discard.
+                pass
+    return SBexamples, SBtargets, catagories
